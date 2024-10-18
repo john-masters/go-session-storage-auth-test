f3e36ab741226da031891cba0ca4c4a6bb1472e4
John Masters <mastersjohnr@gmail.com>
2024-10-18 15:36:55 +1100

    feat: add sessions table to db init

--- db/init.go
+++ db/init.go
@@ -13,12 +13,19 @@ func Init() {
 	defer db.Close()
 	_, err = db.Exec(`
 		DROP TABLE IF EXISTS users;
+		DROP TABLE IF EXISTS sessions;
 
-		CREATE TABLE IF NOT EXISTS users (
+		CREATE TABLE users (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			email TEXT NOT NULL,
 			password TEXT NOT NULL,
 			is_member BOOLEAN NOT NULL DEFAULT FALSE,
+		);
+
+		CREATE TABLE sessions (
+			id TEXT PRIMARY KEY,
+			user_id INTEGER NOT NULL,
+			FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 		);
 	`)
 
